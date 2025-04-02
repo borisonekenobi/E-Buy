0ab7c35cde62eb13146b9a67dbfa763d119c6aa7
tt1ffany <ttrinh32@uwo.ca>
2025-04-02 19:07:47 -0400

    Create create-post page structure and apply styling

---
+++ src/app/pages/create-post/create-post.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-create-post',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './create-post.page.html',
+  styleUrl: './create-post.page.css'
+})
+
+export class CreatePostPage {
+
+}
