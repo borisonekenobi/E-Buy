51dd7b410eeb546966035f935a9bcf5c96d4e855
tt1ffany <ttrinh32@uwo.ca>
2025-04-02 19:07:16 -0400

    Create signup page structure and apply styling

---
+++ src/app/pages/signup/signup.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './signup.page.html',
+  styleUrl: './signup.page.css'
+})
+
+export class SignUpPage {
+
+}
