3f230250a641bc850c32701045c45c8b75902351
Boris Vasilev <borisonekenobi@gmail.com>
2025-04-03 10:06:13 -0400

    Reorder imports to match routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePage} from './pages/home/home.page';
-import {AuctionPage} from './pages/auction/auction.page';
-import {BuyPage} from './pages/buy/buy.page';
 import {LoginPage} from './pages/login/login.page';
 import {SignUpPage} from './pages/signup/signup.page';
-import {CreatePostPage} from './pages/create-post/create-post.page';
-import {BuyDetailsPage} from './pages/buy-details/buy-details.page';
-import {
-  AuctionDetailsPage,
-} from './pages/auction-details/auction-details.page';
-
 import {ProfilePage} from './pages/profile/profile.page';
+import {CreatePostPage} from './pages/create-post/create-post.page';
+import {AuctionPage} from './pages/auction/auction.page';
+import {AuctionDetailsPage} from './pages/auction-details/auction-details.page';
+import {BuyPage} from './pages/buy/buy.page';
+import {BuyDetailsPage} from './pages/buy-details/buy-details.page';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
